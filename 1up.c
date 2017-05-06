e8a49a8ce8e79c6ae0eabc4107381584070499b3
rw-r-r_0644 <rwrr0644@gmail.com>
2017-05-06 10:30:05 -0400

    add SDL drawing functions to display onscreen

--- 1up.c
+++ 1up.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include "draw.h"
+
+pthread_t graphics_thread;
+
+void * graphics_threadf()
+{
+	screen_init("GBA Screen");
+
+ 	while(1)
+ 	{
+ 		screen_clear();
+
+ 		for (int x = 0; x < 240; x++)
+ 			for (int y = 0; y < 160; y++)
+			{
+				char * base_addr = (char *)((0x6000000 + x + (240 * y)) * 3);
+ 				draw_pixel(x, y, *base_addr, *(base_addr + 1), *(base_addr + 2));
+			}
+ 		screen_flip();
+ 	}
+	
+	screen_end();
+
+	return NULL;
+}
 
 int init()
 {
@@ -10,6 +36,8 @@ int init()
 
 	// map virtual memory for screen buffer
 	mmap((void*)0x6000000, 0x9600*3, PROT_WRITE | PROT_READ, MAP_ANONYMOUS | MAP_SHARED | MAP_FIXED, -1, 0);
+
+	pthread_create(&graphics_thread, NULL, graphics_threadf, NULL);
 }
 
 int deinit()
@@ -28,5 +56,5 @@ int deinit()
 	printf("Wrote screenshot to screen.ppm\n");
 
 	// exit syscall to stop gracefully
-	exit(0);
+	while(1);
 }
---
+++ draw.h
@@ -0,0 +1,24 @@
+
+#include <stdint.h>
+
+// change these to size of the screen:
+#define screen_x 300
+#define screen_y 200
+
+extern uint8_t * screen; // bgr
+
+void screen_init(const char * title);
+void screen_end();
+void screen_clear();
+void screen_flip();
+
+void * render_text(char * text, char r, char g, char b);
+void draw_text(int x, int y, void * render);
+
+void draw_pixel(int x, int y, char r, char g, char b);
+void draw_line(int x0, int y0, int x1, int y1, char r, char g, char b);
+void draw_rect(int x1, int y1, int x2, int y2, char r, char g, char b);
+void draw_fill_rect(int x1, int y1, int x2, int y2, char r, char g, char b);
+void draw_circle(int xCen, int yCen, int radius, char r, char g, char b);
+void draw_fill_circle(int xCen, int yCen, int radius, char r, char g, char b);
+void draw_circle_circum(int cx, int cy, int x, int y, char r, char g, char b);
